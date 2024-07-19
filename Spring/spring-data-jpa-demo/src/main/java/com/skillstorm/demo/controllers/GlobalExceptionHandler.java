0689a3cc972f4e3492d7b831492399243c8da996
ericahicks <48265629+ericahicks@users.noreply.github.com>
2024-07-19 12:18:53 -0400

    exceptionhandler in restcontrolleradvice

--- Spring/spring-data-jpa-demo/src/main/java/com/skillstorm/demo/controllers/GlobalExceptionHandler.java
+++ Spring/spring-data-jpa-demo/src/main/java/com/skillstorm/demo/controllers/GlobalExceptionHandler.java
@@ -8,9 +8,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    // @ExceptionHandler(MethodArgumentNotValidException.class) 
-    // public ResponseEntity<Object> handleValidationError(Exception e) {
-    //     return ResponseEntity.badRequest().body(e.getMessage());
-    // }
+    /*
+    Think of this like a giant try-catch surrounding you rentire controllers
+    
+    Specify the exceptions you want to catch,
+
+    Now you can c hange the HTTP Response message
+     
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class) 
+    public ResponseEntity<Object> handleValidationError(Exception e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
 
 }
