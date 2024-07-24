230942a43bc23b4fa905e03e2080d3138687adeb
ericahicks <48265629+ericahicks@users.noreply.github.com>
2024-07-24 10:53:09 -0400

    adding js files

--- HTML-CSS-JS/js/scripts/01-dom.-manipulation.js
+++ HTML-CSS-JS/js/scripts/01-dom.-manipulation.js
@@ -7,5 +7,28 @@ document.getElementById("1").textContent = "Hello!"
 // we can create callback functions that execute after a set amount of time (or repeatedly at an interval)
 
 setTimeout(() => {
+    
+    clearInterval(intervalId)
     document.getElementById("1").textContent = ("5 secs has passed");
 }, 5000)
+
+// javascript loosely typed 
+// variable declarations use var, let, or const
+var x = 0;
+let intervalId = setInterval(() => {
+    x = x + 1;
+    console.log(x);
+    document.getElementById("2").textContent = x;
+}, 1000); // miliseconds
+
+// event handlers and callback functions
+// add Event listener to a button
+// change the color of the div to blue
+let btn = document.querySelector("button"); // assumes first on pg
+btn.addEventListener("click", (e) => {
+    console.dir(e);
+    let greenDiv = document.getElementsByClassName("green")[0];
+    greenDiv.classList.remove("green");
+    greenDiv.classList.add("blue");
+    
+})
---
+++ HTML-CSS-JS/js/scripts/02-variables.js
@@ -0,0 +1,7 @@
+// Three ways to declare a variable
+var a = 1;
+let b = 2;
+const c = 3;
+// c = 4; // TypeError: Assignment to constant variable.
+console.log(`a = ${a} b = ${b} c = ${c}`);
+
